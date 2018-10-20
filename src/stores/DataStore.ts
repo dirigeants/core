8b6ac0a4edee17a76c2d89f8989cd914d2e8c89d
kyranet <kyradiscord@gmail.com>
2018-10-20 02:13:07 +0200

    feat: DataStore and Base structures

---
+++ src/stores/DataStore.ts
@@ -0,0 +1,43 @@
+import Client from '../client/Client';
+import Base from '../structures/Base';
+
+export default class DataStore<Piece extends Base, VConstructor extends new (...args) => Piece> extends Map<string, Piece> {
+	public client: Client;
+	private holds: VConstructor;
+
+	public constructor(client: Client, holds: VConstructor, iterable?: Iterable<Piece>) {
+		super();
+
+		this.client = client;
+		this.holds = holds;
+
+		if (iterable) for (const item of iterable) this.add(item);
+	}
+
+	// tslint:disable-next-line no-any
+	public add(data: { id: string, [k: string]: any }, cache: boolean = true) {
+		const existing = this.get(data.id);
+		if (existing) return existing.patch(data);
+
+		const entry = new this.holds(this.client, data);
+		if (cache) this.set(entry.id, entry);
+		return entry;
+	}
+
+	public resolve(data: Piece | string): Piece | null {
+		if (typeof data === 'string') return this.get(data) || null;
+		if (data instanceof this.holds) return data;
+		return null;
+	}
+
+	public resolveID(data: Piece | string): string | null {
+		if (typeof data === 'string') return data;
+		if (data instanceof this.holds) return data.id;
+		return null;
+	}
+
+	public static get [Symbol.species](): typeof Map {
+		return Map;
+	}
+
+}
---
+++ src/structures/Base.ts
@@ -0,0 +1,21 @@
+import Client from '../client/Client';
+
+export default abstract class Base {
+	public readonly client: Client;
+	public abstract readonly id: string;
+
+	public constructor(client: Client) {
+		this.client = client;
+	}
+
+	public clone<T = Base>(): T {
+		return Object.assign(Object.create(this), this);
+	}
+
+	public abstract patch(data: object): this;
+
+	public valueOf(): string {
+		return this.id;
+	}
+
+}
