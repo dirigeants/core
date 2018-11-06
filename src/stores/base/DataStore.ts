84f7b3661b8e385c560510b843e03c00e9a2556e
Unknown <bdistin@gmail.com>
2018-11-05 19:39:40 -0600

    use Cache in DataStore

--- src/stores/base/DataStore.ts
+++ src/stores/base/DataStore.ts
@@ -1,12 +1,13 @@
 import Client from '../../client/Client';
 import Base from '../../structures/base/Base';
+import Cache from '../../util/Cache';
 
-export default class DataStore<Piece extends Base, VConstructor extends new (...args) => Piece> extends Map<string, Piece> {
+export default class DataStore<Structure extends Base, VConstructor extends new (...args) => Structure> extends Cache<string, Structure> {
 
 	public client: Client;
 	private holds: VConstructor;
 
-	public constructor(client: Client, holds: VConstructor, iterable?: Iterable<Piece>) {
+	public constructor(client: Client, holds: VConstructor, iterable?: Iterable<Structure>) {
 		super();
 
 		this.client = client;
@@ -25,20 +26,20 @@ export default class DataStore<Piece extends Base, VConstructor extends new (...
 		return entry;
 	}
 
-	public resolve(data: Piece | string): Piece | null {
+	public resolve(data: Structure | string): Structure | null {
 		if (typeof data === 'string') return this.get(data) || null;
 		if (data instanceof this.holds) return data;
 		return null;
 	}
 
-	public resolveID(data: Piece | string): string | null {
+	public resolveID(data: Structure | string): string | null {
 		if (typeof data === 'string') return data;
 		if (data instanceof this.holds) return data.id;
 		return null;
 	}
 
-	public static get [Symbol.species](): typeof Map {
-		return Map;
+	public static get [Symbol.species](): typeof Cache {
+		return Cache;
 	}
 
 }
