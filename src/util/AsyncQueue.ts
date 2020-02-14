f401bf8048e1d85d88b7d22665a71a73a80252b8
Antonio Román <kyradiscord@gmail.com>
2020-02-14 23:57:56 +0100

    feat: Add AsyncQueue

---
+++ src/util/AsyncQueue.ts
@@ -0,0 +1,60 @@
+/**
+ * The AsyncQueue class used to sequentialize burst requests
+ */
+export class AsyncQueue {
+
+	/**
+	 * The promises array
+	 */
+	private promises: InternalAsyncQueueDeferredPromise[] = [];
+
+	/**
+	 * Waits for last promise and queues a new one
+	 * @example
+	 * const queue = new AsyncQueue();
+	 * async function request(url, options) {
+	 *     await queue.wait();
+	 *     try {
+	 *         const result = await fetch(url, options);
+	 *         // Do some operations with 'result'
+	 *     } finally {
+	 *         // Remove first entry from the queue and resolve for the next entry
+	 *         queue.shift();
+	 *     }
+	 * }
+	 *
+	 * request(someUrl1, someOptions1); // Will call fetch() immediately
+	 * request(someUrl2, someOptions2); // Will call fetch() after the first finished
+	 * request(someUrl3, someOptions3); // Will call fetch() after the second finished
+	 */
+	public async wait() {
+		if (this.promises.length > 0) {
+			const last = this.promises[this.promises.length - 1];
+
+			const promise = new Promise<void>(resolve => {
+				this.promises.push({
+					resolve,
+					promise
+				});
+			});
+
+			await last.promise;
+		}
+	}
+
+	/**
+	 * Frees the queue's lock for the next item to process
+	 */
+	public shift() {
+		const deferred = this.promises.shift();
+		if (typeof deferred !== 'undefined') deferred.resolve();
+	}
+}
+
+/**
+ * @internal
+ */
+interface InternalAsyncQueueDeferredPromise {
+	resolve(): void;
+	promise: Promise<void>;
+}
