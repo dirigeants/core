413af2c2e90196576f544b71d9d023de6e849130
bdistin <bdistin@gmail.com>
2020-02-20 16:36:36 -0600

    fix a wrong import

--- src/client/BaseClient.ts
+++ src/client/BaseClient.ts
@@ -1,7 +1,7 @@
 import { EventEmitter } from 'events';
 import { mergeDefault } from '@klasa/utils';
 import { REST } from './rest/REST';
-import { ClientOptionsDefaults } from '../util/Constants';
+import { BaseClientOptionsDefaults } from '../util/Constants';
 
 import type { RESTOptions } from './rest/RESTManager';
 import { TimerManager } from '../util/TimerManager';
@@ -31,7 +31,7 @@ export class BaseClient extends EventEmitter {
 	 */
 	public constructor(options: Partial<BaseClientOptions>) {
 		super();
-		this.options = mergeDefault(ClientOptionsDefaults, options);
+		this.options = mergeDefault(BaseClientOptionsDefaults, options);
 		this.api = new REST(this.options.rest)
 			.on('debug', this.emit.bind(this, 'debug'))
 			.on('ratelimited', this.emit.bind(this, 'ratelimited'));
