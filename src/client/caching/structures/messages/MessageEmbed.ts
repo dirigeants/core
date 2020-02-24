8c673ebb3a9be2033c8833be2e3644f56286b617
Gryffon Bellish <owenbellish@gmail.com>
2020-02-24 09:11:39 -0500

    Fix mismatch types

--- src/client/caching/structures/messages/MessageEmbed.ts
+++ src/client/caching/structures/messages/MessageEmbed.ts
@@ -1,6 +1,6 @@
 import { deepClone } from '@klasa/utils';
 
-import type { APIEmbedData, APIEmbedFieldData, APIEmbedProviderData, EmbedType, APIEmbedAuthorData, APIEmbedFooterData, APIEmbedImageData } from '../../../../util/types/DiscordAPI';
+import type { APIEmbedData, APIEmbedFieldData, APIEmbedProviderData, EmbedType, APIEmbedAuthorData, APIEmbedFooterData, APIEmbedImageData, APIEmbedVideoData } from '../../../../util/types/DiscordAPI';
 
 export interface StringResolvable {
 	toString(): string;
@@ -59,7 +59,7 @@ export class MessageEmbed implements APIEmbedData {
 	/**
 	 * Received video data
 	 */
-	public video?: APIEmbedImageData;
+	public video?: APIEmbedVideoData;
 
 	/**
 	 * The embed author data
