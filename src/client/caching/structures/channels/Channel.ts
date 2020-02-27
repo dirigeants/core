11fde244d0fa853e4a6d81a05279e54cb985e047
Gryffon Bellish <owenbellish@gmail.com>
2020-02-27 11:08:40 -0500

    partial stuff

---
+++ src/client/caching/structures/channels/Channel.ts
@@ -0,0 +1,16 @@
+import { Structure } from "../base/Structure";
+
+import type { APIChannelPartial}  from '../../../../util/types/DiscordAPI';
+import type { Client } from "../../../Client";
+
+export abstract class Channel extends Structure {
+    public id: string;
+
+    public constructor(client: Client, data: APIChannelPartial) {
+        super(client);
+
+        this.id = data.id;
+    }
+}
+
+export interface TextBasedChannel {}
