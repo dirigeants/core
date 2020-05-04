fb2f57aa7f28f8a4a9cd9897dfaef0e93c4e7e87
Jacz <itsmejacz@gmail.com>
2020-05-04 21:49:31 +1000

    The start of the Message structure pieces

--- src/client/caching/structures/Message.ts
+++ src/client/caching/structures/Message.ts
@@ -3,11 +3,15 @@ import { Structure } from './base/Structure';
 import type { Client } from '../../Client';
 import type { WebhookClient } from '../../WebhookClient';
 import type { APIMessageData } from '@klasa/dapi-types';
+import { MessageMentions } from './MessageMentions';
+import { MessageEmbed } from './messages/MessageEmbed';
 
 export class Message extends Structure {
 
 	public id: string;
 	public content!: string;
+
+	public embeds: MessageEmbed[] = [];
 
 	public constructor(client: Client | WebhookClient, data: APIMessageData) {
 		super(client);
@@ -18,8 +22,39 @@ export class Message extends Structure {
 	}
 
 	protected _patch(data: APIMessageData): this {
+		this.channelID = data.channel_id;
+
+		// TODO: More logic here
+		this.guildID = data.guild_id;
+
+		this.author = data.author;
+
+		// TODO: When Guild, GuildStore & GuildMemberStore is done, rewrite this
+		this.member = data.member;
+
 		this.content = data.content;
-		// todo: fill in logic
+
+		this.timestamp = data.timestamp;
+
+		this.edited_timestamp = data.edited_timestamp;
+
+		this.tts = data.tts;
+
+		this.mentions = new MessageMentions(this, data.mentions, data.mention_roles, data.mention_channels, data.mention_everyone);
+
+		// TODO: Implement Attachment
+		this.attachments = data.attachments;
+
+		if (data.embeds) for (const embed of data.embeds) this.embeds.push(new MessageEmbed(embed));
+
+		this.reactions = data.reactions;
+
+		this.nonce = data.nonce;
+
+		this.pinned = data.pinned;
+
+		this.type = data.type;
+
 		return this;
 	}
 
