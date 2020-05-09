0fee2ef34dfb8f7d12eb558d43457797efdc7fac
Antonio Román <kyradiscord@gmail.com>
2020-05-09 15:10:54 +0200

    fix(Extender): address @bdistin's feedback

--- src/client/caching/Structures.ts
+++ src/client/caching/Structures.ts
@@ -1,11 +1,15 @@
 import { Cache } from '@klasa/cache';
 
-export class Extender<T extends ExtenderStructures> extends Cache<keyof T, T[keyof T]> {
+class Extender<T extends ExtenderStructures> extends Cache<keyof T, T[keyof T]> {
 
 	public add<K extends string, V>(key: K, fn: V): Extender<Added<T, K, V>> {
 		if (super.has(key as keyof T)) throw new Error(`The structure ${key} already exists.`);
 		super.set(key as keyof T, fn as unknown as T[keyof T]);
 		return this as Extender<Added<T, K, V>>;
+	}
+
+	public set(_key: keyof T, _value: T[keyof T]) {
+		throw new Error('Cannot set keys to this extender.');
 	}
 
 	public get<K extends keyof T>(key: K): T[K] | undefined {
@@ -15,11 +19,13 @@ export class Extender<T extends ExtenderStructures> extends Cache<keyof T, T[key
 	public extend<K extends keyof T, R extends T[K]>(key: K, fn: ExtenderCallback<T[K], R>): Extender<Extended<T, K, R>> {
 		const structure = this.get(key);
 		if (typeof structure === 'undefined') throw new TypeError(`The structure ${key} does not exist.`);
-		this.set(key, fn(structure));
+		super.set(key, fn(structure));
 		return this as unknown as Extender<Extended<T, K, R>>;
 	}
 
 }
+
+export const extender = new Extender();
 
 export interface ExtenderStructures {
 	[K: string]: never;
