555df23b5ffd298cc1020d8c1e87324a57e88656
Antonio Román <kyradiscord@gmail.com>
2020-05-16 11:01:56 +0200

    fix(GUILD_BAN_REMOVE): always build

--- src/actions/guilds/bans/GUILD_BAN_REMOVE.ts
+++ src/actions/guilds/bans/GUILD_BAN_REMOVE.ts
@@ -1,17 +1,18 @@
 import { Action } from '../../../lib/structures/Action';
+import { extender } from '../../../util/Extender';
 
 import type { GuildBanAddDispatch } from '@klasa/ws';
 import type { Ban } from '../../../client/caching/structures/guilds/Ban';
 
 export default class CoreAction extends Action {
 
-	public check(data: GuildBanAddDispatch): Ban | null {
-		const guild = this.client.guilds.get(data.d.guild_id);
-		return guild?.bans.get(data.d.user.id) ?? null;
+	public check(): null {
+		return null;
 	}
 
-	public build(): null {
-		return null;
+	public build(data: GuildBanAddDispatch): Ban | null {
+		const guild = this.client.guilds.get(data.d.guild_id);
+		return guild ? new (extender.get('Ban'))(this.client, data.d, guild) : null;
 	}
 
 	public cache(data: Ban): void {
