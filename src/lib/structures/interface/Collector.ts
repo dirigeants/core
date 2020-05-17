d4b2f0463479c3935081bd0a98c15e04236d739f
Gryffon Bellish <owenbellish@gmail.com>
2020-05-17 17:34:04 -0400

    collector filter needs to be sync

--- src/lib/structures/interface/Collector.ts
+++ src/lib/structures/interface/Collector.ts
@@ -1,6 +1,6 @@
 import type { EventEmitter } from 'events';
 
-export type CollectorFilter<K, V> = (value: V, key: K, collected: [K, V][]) => boolean | Promise<boolean>;
+export type CollectorFilter<K, V> = (value: V, key: K, collected: [K, V][]) => boolean;
 
 export interface CollectorOptions<K, V> {
 	event: string;
@@ -37,7 +37,7 @@ export abstract class Collector<K, V> implements AsyncIterableIterator<[K, V]> {
 	}
 
 	public push(key: K, value: V): void {
-		this.#queue.push([key, value]);
+		if (this.filter(value, key, this.queue)) this.#queue.push([key, value]);
 	}
 
 	public end(): void {
@@ -46,13 +46,7 @@ export abstract class Collector<K, V> implements AsyncIterableIterator<[K, V]> {
 	}
 
 	public async next(): Promise<IteratorResult<[K, V]>> {
-		if (this.#queue.length) {
-			const value = this.#queue.shift() as [K, V];
-			if (await this.filter(value[1], value[0], this.queue)) {
-				this.#collected++;
-				return { done: false, value };
-			}
-		}
+		if (this.#queue.length) return { done: false, value: this.#queue.shift() as [K, V] };
 		if (this.ended) return { done: true, value: undefined as never };
 		return new Promise<IteratorResult<[K, V]>>((resolve): void => {
 			this.emitter.once(this.event, (): void => {
