670606c86c5ea021586163d03fb5a1c5a8636d8c
Gryffon Bellish <owenbellish@gmail.com>
2020-05-17 17:32:14 -0400

    lint

--- src/lib/structures/interface/Collector.ts
+++ src/lib/structures/interface/Collector.ts
@@ -3,64 +3,66 @@ import type { EventEmitter } from 'events';
 export type CollectorFilter<K, V> = (value: V, key: K, collected: [K, V][]) => boolean | Promise<boolean>;
 
 export interface CollectorOptions<K, V> {
-    event: string;
-    filter?: CollectorFilter<K, V>;
+	event: string;
+	filter?: CollectorFilter<K, V>;
 }
 
 export abstract class Collector<K, V> implements AsyncIterableIterator<[K, V]> {
-    public ended = false;
 
-    public filter: CollectorFilter<K, V>;
+	public ended = false;
 
-    public event: string;
+	public filter: CollectorFilter<K, V>;
 
-    #queue: [K, V][] = [];
+	public event: string;
 
-    #collected: number = 0;
+	#queue: [K, V][] = [];
 
-    public constructor(public readonly emitter: EventEmitter, options: CollectorOptions<K, V>) {
-        this.filter = options.filter ?? ((): boolean => true);
+	#collected = 0;
 
-        this.event = options.event;
+	public constructor(public readonly emitter: EventEmitter, options: CollectorOptions<K, V>) {
+		this.filter = options.filter ?? ((): boolean => true);
 
-        this.push = this.push.bind(this);
-        this.emitter.on(this.event, this.push);
-    }
+		this.event = options.event;
 
-    public get collected(): number {
-        return this.#collected;
-    }
+		this.push = this.push.bind(this);
+		this.emitter.on(this.event, this.push);
+	}
 
-    public get queue(): [K, V][] {
-        return this.#queue.slice();
-    }
+	public get collected(): number {
+		return this.#collected;
+	}
 
-    public push(key: K, value: V): void {
-        this.#queue.push([key, value]);
-    }
+	public get queue(): [K, V][] {
+		return this.#queue.slice();
+	}
 
-    public end(): void {
-        this.ended = true;
-        this.emitter.off(this.event, this.push);
-    }
+	public push(key: K, value: V): void {
+		this.#queue.push([key, value]);
+	}
 
-    public async next(): Promise<IteratorResult<[K, V]>> {
-        if (this.#queue.length) {
-            const value = this.#queue.shift() as [K, V];
-            if (await this.filter(value[1], value[0], this.queue)) {
-                this.#collected++;
-                return { done: false, value };
-            }
-        }
-        if (this.ended) return { done: true, value: undefined as never };
-        return new Promise<IteratorResult<[K, V]>>((resolve): void => {
-            this.emitter.once((this.event), (): void => {
-                resolve(this.next());
-            });
-        });
-    }
+	public end(): void {
+		this.ended = true;
+		this.emitter.off(this.event, this.push);
+	}
 
-    public [Symbol.asyncIterator](): AsyncIterableIterator<[K, V]> {
-        return this;
-    }
+	public async next(): Promise<IteratorResult<[K, V]>> {
+		if (this.#queue.length) {
+			const value = this.#queue.shift() as [K, V];
+			if (await this.filter(value[1], value[0], this.queue)) {
+				this.#collected++;
+				return { done: false, value };
+			}
+		}
+		if (this.ended) return { done: true, value: undefined as never };
+		return new Promise<IteratorResult<[K, V]>>((resolve): void => {
+			this.emitter.once(this.event, (): void => {
+				resolve(this.next());
+			});
+		});
+	}
+
+	public [Symbol.asyncIterator](): AsyncIterableIterator<[K, V]> {
+		return this;
+	}
+
 }
