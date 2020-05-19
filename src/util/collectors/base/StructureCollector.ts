8137a8eee6aecb3fb224d23db766c4176eb4a451
bdistin <bdistin@gmail.com>
2020-05-19 09:01:40 -0500

    StructureIterator was a useless class

--- src/util/collectors/base/StructureCollector.ts
+++ src/util/collectors/base/StructureCollector.ts
@@ -1,9 +1,9 @@
 import { Cache } from '@klasa/cache';
 
+import type { EventIterator } from '@klasa/event-iterator';
 import type { Structure } from '../../../client/caching/structures/base/Structure';
-import type { StructureIterator } from '../../iterators/base/StructureIterator';
 
-export class StructureCollector<T extends Structure, I extends StructureIterator<T>> {
+export class StructureCollector<T extends Structure, I extends EventIterator<T>> {
 
 	protected collected = new Cache<string, T>();
 
--- src/util/iterators/ReactionIterator.ts
+++ src/util/iterators/ReactionIterator.ts
@@ -1,19 +1,18 @@
 import { EventIterator, EventIteratorOptions } from '@klasa/event-iterator';
 import { Message } from '../../client/caching/structures/Message';
-import { StructureIterator } from './base/StructureIterator';
 import { ClientEvents } from '../types/Util';
 import { MessageReaction } from '../../client/caching/structures/messages/reactions/MessageReaction';
 
-export class ReactionIterator extends StructureIterator<MessageReaction> {
+export class ReactionIterator extends EventIterator<MessageReaction> {
 
 	public constructor(message: Message, options: EventIteratorOptions<MessageReaction> = {}) {
 		const { limit, idle, filter = (): boolean => true } = options;
 
-		super(new EventIterator(message.client, ClientEvents.MessageReactionAdd, {
+		super(message.client, ClientEvents.MessageReactionAdd, {
 			limit,
 			idle,
 			filter: (react): boolean => react.message === message && filter(react)
-		}));
+		});
 	}
 
 }
