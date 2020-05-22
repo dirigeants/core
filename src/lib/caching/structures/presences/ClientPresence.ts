81f9c1917fde4b37346032d4bd61bea84085fb05
Antonio Román <kyradiscord@gmail.com>
2020-05-22 17:41:08 +0200

    feat(ClientPresence): allow shard selection

--- src/lib/caching/structures/presences/ClientPresence.ts
+++ src/lib/caching/structures/presences/ClientPresence.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-dupe-class-members */
-import { OpCodes } from '@klasa/ws';
+import { OpCodes, SendPayload } from '@klasa/ws';
 import { Presence } from '../guilds/Presence';
 import { PresenceBuilder, StatusUpdateData } from './PresenceBuilder';
 
@@ -16,19 +16,35 @@ export class ClientPresence extends Presence {
 	 * @param presence The presence data to be sent.
 	 * @see https://discord.com/developers/docs/topics/gateway#update-status
 	 */
-	public modify(game: StatusUpdateData): this;
+	public modify(game: StatusUpdateData, shards?: number | number[]): this;
 	/**
 	 * Sets the client presence with a builder, and returns it.
 	 * @since 0.0.1
 	 * @param builder The builder to aid building the game.
 	 */
-	public modify(builder: (presence: PresenceBuilder) => PresenceBuilder): this;
-	public modify(presence: StatusUpdateData | ((game: PresenceBuilder) => PresenceBuilder)): this {
+	public modify(builder: (presence: PresenceBuilder) => PresenceBuilder, shards?: number | number[]): this;
+	public modify(presence: StatusUpdateData | ((game: PresenceBuilder) => PresenceBuilder), shards?: number | number[]): this {
 		const data = typeof presence === 'function' ? presence(new PresenceBuilder()) : presence;
+		// eslint-disable-next-line id-length
+		const sent: SendPayload = { op: OpCodes.STATUS_UPDATE, d: data };
 
-		for (const shard of this.client.ws.shards.values()) {
-			// eslint-disable-next-line id-length
-			shard.send({ op: OpCodes.STATUS_UPDATE, d: data });
+		// No shards specified
+		if (typeof shards === 'undefined') {
+			for (const shard of this.client.ws.shards.values()) shard.send(sent);
+			return this;
+		}
+
+		// One shard specified
+		if (typeof shards === 'number') {
+			const shard = this.client.ws.shards.get(shards);
+			if (shard) shard.send(sent);
+			return this;
+		}
+
+		// Multiple shards specified
+		for (const shardID of shards) {
+			const shard = this.client.ws.shards.get(shardID);
+			if (shard) shard.send(sent);
 		}
 
 		return this;
