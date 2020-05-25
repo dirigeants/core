df9fc68427289b6322b0672d9abdf6b56a2f377d
bdistin <bdistin@gmail.com>
2020-05-25 18:58:46 -0500

    fix guild channels?

--- src/lib/caching/structures/guilds/Guild.ts
+++ src/lib/caching/structures/guilds/Guild.ts
@@ -520,8 +520,8 @@ export class Guild extends Structure {
 
 		if (data.channels) {
 			this.channels.clear();
-			// eslint-disable-next-line dot-notation
-			for (const channel of data.channels) this.client.channels['_add'](channel);
+			// eslint-disable-next-line dot-notation, @typescript-eslint/camelcase
+			for (const channel of data.channels) this.client.channels['_add']({ ...channel, guild_id: this.id });
 		}
 
 		this.features = data.features;
