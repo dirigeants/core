15b3e6bb087f5146d1b2d10069142791ea3570a7
bdistin <bdistin@gmail.com>
2020-05-25 18:53:09 -0500

    ensure guild shard on update, and fix client#channels?

--- src/lib/caching/structures/guilds/Guild.ts
+++ src/lib/caching/structures/guilds/Guild.ts
@@ -521,7 +521,7 @@ export class Guild extends Structure {
 		if (data.channels) {
 			this.channels.clear();
 			// eslint-disable-next-line dot-notation
-			for (const channel of data.channels) this.channels['_add'](channel);
+			for (const channel of data.channels) this.client.channels['_add'](channel);
 		}
 
 		this.features = data.features;
