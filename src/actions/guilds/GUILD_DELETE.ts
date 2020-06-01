e00ffa800374aa68a4772764751d86dc5aecd2bd
Antonio Román <kyradiscord@gmail.com>
2020-06-01 18:28:11 +0200

    feat(Events): added guild{Una|A}vailable

--- src/actions/guilds/GUILD_DELETE.ts
+++ src/actions/guilds/GUILD_DELETE.ts
@@ -1,20 +1,49 @@
-import { Action, Guild } from '@klasa/core';
+import { Action, ClientEvents, extender } from '@klasa/core';
 
 import type { GuildDeleteDispatch } from '@klasa/ws';
 
 export default class CoreAction extends Action {
 
-	public check(data: GuildDeleteDispatch): Guild | null {
-		return this.client.guilds.get(data.d.id) ?? null;
+	/**
+	 * Processes the event data from the websocket.
+	 * @since 0.0.1
+	 * @param data The raw data from {@link Client#ws}
+	 */
+	public run(data: GuildDeleteDispatch): void {
+		const guild = this.client.guilds.get(data.d.id);
+		if (!guild) {
+			if (!data.d.unavailable) return;
+
+			const created = new (extender.get('Guild'))(this.client, data.d, data.shard_id);
+			if (this.client.options.cache.enabled) {
+				this.client.guilds.set(created.id, created);
+			}
+
+			this.client.emit(ClientEvents.GuildUnavailable, created);
+			return;
+		}
+
+		if (data.d.unavailable) {
+			guild.unavailable = true;
+			this.client.emit(ClientEvents.GuildUnavailable, guild);
+		} else {
+			guild.unavailable = false;
+			guild.deleted = true;
+			this.client.guilds.delete(guild.id);
+			this.client.emit(ClientEvents.GuildDelete, guild);
+		}
+	}
+
+	public check(): null {
+		return null;
 	}
 
 	public build(): null {
 		return null;
 	}
 
-	public cache(data: Guild): void {
-		data.deleted = true;
-		this.client.guilds.delete(data.id);
+	public cache(): void {
+		// noop
 	}
 
 }
