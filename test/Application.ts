aec2514630fe60b50342bdf502c1e75550a9934e
Skillz <gankstarsleague@gmail.com>
2020-06-10 22:26:51 -0400

    fix test

--- test/Application.ts
+++ test/Application.ts
@@ -105,6 +105,6 @@ ava('fetch application', async (test): Promise<void> => {
 
 	// Test guild availability
 	test.is(application.guild, null);
-	const guild = client.guilds._add(rawGuild);
+	const guild = client.guilds['_add'](rawGuild);
 	test.is(application.guild, guild);
 });
