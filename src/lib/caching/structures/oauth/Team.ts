ffa0666f135a1d637c7cded2ee7e2f0be8d67af9
Skillz <gankstarsleague@gmail.com>
2020-06-10 21:57:09 -0400

    add iconrul to team

--- src/lib/caching/structures/oauth/Team.ts
+++ src/lib/caching/structures/oauth/Team.ts
@@ -1,51 +1,59 @@
-import { Cache } from '@klasa/cache';
-import { TeamMember } from './TeamMember';
+import { Cache } from "@klasa/cache";
+import { TeamMember } from "./TeamMember";
 
-import type { APITeamData } from '@klasa/dapi-types';
-import type { Client } from '../../../client/Client';
+import type { APITeamData } from "@klasa/dapi-types";
+import type { Client } from "../../../client/Client";
+import { ImageURLOptions } from "@klasa/rest";
 
 /**
  * @see https://discord.com/developers/docs/topics/teams#data-models-team-object
  */
 export class Team {
+  /**
+   * The unique id of the team.
+   * @since 0.0.1
+   */
+  public readonly id: string;
+
+  /**
+   * A hash of the image of the team's icon.
+   * @since 0.0.1
+   */
+  public icon?: string;
+
+  /**
+   * The members of the team.
+   * @since 0.0.1
+   */
+  public members: Cache<string, TeamMember>;
+
+  /**
+   * The user id of the current team owner.
+   * @since 0.0.1
+   */
+  public ownerID: string;
+
+  public constructor(public readonly client: Client, data: APITeamData) {
+    this.id = data.id;
+    this.icon = data.icon;
+    this.members = new Cache(data.members.map((member) => [member.user.id, new TeamMember(client, member)]));
+    this.ownerID = data.owner_user_id;
+  }
+
+  /**
+   * The owner of this Team
+   * @since 0.0.4
+   */
+  public get owner(): TeamMember {
+    return this.members.get(this.ownerID) as TeamMember;
+  }
 
 	/**
-	 * The unique id of the team.
-	 * @since 0.0.1
+	 * Returns the teams icon url if available.
+	 * @param options The image size, format, and other image url options.
 	 */
-	public readonly id: string;
-
-	/**
-	 * A hash of the image of the team's icon.
-	 * @since 0.0.1
-	 */
-	public icon?: string;
-
-	/**
-	 * The members of the team.
-	 * @since 0.0.1
-	 */
-	public members: Cache<string, TeamMember>;
-
-	/**
-	 * The user id of the current team owner.
-	 * @since 0.0.1
-	 */
-	public ownerID: string;
-
-	public constructor(public readonly client: Client, data: APITeamData) {
-		this.id = data.id;
-		this.icon = data.icon;
-		this.members = new Cache(data.members.map(member => [member.user.id, new TeamMember(client, member)]));
-		this.ownerID = data.owner_user_id;
-	}
-
-	/**
-	 * The owner of this Team
-	 * @since 0.0.4
-	 */
-	public get owner(): TeamMember {
-		return this.members.get(this.ownerID) as TeamMember;
-	}
-
+  public iconURL(options?: ImageURLOptions): string | null {
+    if (!this.icon) return null;
+    return this.client.api.cdn.teamIcon(this.id, this.icon, options);
+  }
 }
