6bd1b394ddf613e57e7fd10656ec7585ed8bca02
Tyler Richards <11968358+tjrgg@users.noreply.github.com>
2020-07-14 06:09:41 -0500

    fix(Guild): memberCount undefined, not patched (#302)
    Co-authored-by: Antonio Román <kyradiscord@gmail.com>

--- src/lib/caching/structures/guilds/Guild.ts
+++ src/lib/caching/structures/guilds/Guild.ts
@@ -361,7 +361,6 @@ export class Guild extends Structure {
 
 		this.unavailable = data.unavailable ?? false;
 		if (!this.unavailable) {
-			this.memberCount = data.member_count ?? null;
 			this._patch(data);
 		}
 	}
@@ -559,6 +558,7 @@ export class Guild extends Structure {
 		this.publicUpdatesChannel = data.public_updates_channel_id;
 		this.approximateMemberCount = data.approximate_member_count;
 		this.approximatePresenceCount = data.approximate_presence_status;
+		this.memberCount = data.member_count ?? this.memberCount ?? null;
 
 		return this;
 	}
