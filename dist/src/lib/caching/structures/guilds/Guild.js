ec6acc1df5d873823b714323c97111be9b5dcd1d
kyranet <kyranet@users.noreply.github.com>
2020-07-14 11:10:14 +0000

    TSC build: 6bd1b394ddf613e57e7fd10656ec7585ed8bca02

--- dist/src/lib/caching/structures/guilds/Guild.js
+++ dist/src/lib/caching/structures/guilds/Guild.js
@@ -22,7 +22,7 @@ const ImageUtil_1 = require("../../../util/ImageUtil");
  */
 class Guild extends Structure_1.Structure {
     constructor(client, data, shardID) {
-        var _a, _b;
+        var _a;
         super(client);
         /**
          * Whether the guild is deleted.
@@ -45,7 +45,6 @@ class Guild extends Structure_1.Structure {
         this.widget = new GuildWidget_1.GuildWidget({ enabled: null, channel_id: null }, this);
         this.unavailable = (_a = data.unavailable) !== null && _a !== void 0 ? _a : false;
         if (!this.unavailable) {
-            this.memberCount = (_b = data.member_count) !== null && _b !== void 0 ? _b : null;
             this._patch(data);
         }
     }
@@ -167,7 +166,7 @@ class Guild extends Structure_1.Structure {
         return this.name;
     }
     _patch(data) {
-        var _a, _b, _c;
+        var _a, _b, _c, _d, _e;
         this.name = data.name;
         this.ownerID = data.owner_id;
         this.region = data.name;
@@ -220,6 +219,7 @@ class Guild extends Structure_1.Structure {
         this.publicUpdatesChannel = data.public_updates_channel_id;
         this.approximateMemberCount = data.approximate_member_count;
         this.approximatePresenceCount = data.approximate_presence_status;
+        this.memberCount = (_e = (_d = data.member_count) !== null && _d !== void 0 ? _d : this.memberCount) !== null && _e !== void 0 ? _e : null;
         return this;
     }
 }
